457ff50c7efabe544e73abc80824911bb8199f33
Kwaku Opoku-Ware <94206249+Kwakuopokuware401@users.noreply.github.com>
2021-11-15 16:59:52 +0100

    Create Sayiloveyou.py

---
+++ Sayiloveyou.py
@@ -0,0 +1,8 @@
+# Ask the user for their name
+name = input("What is your name? ")
+
+
+# Print their name 100 times as well as i love you
+for x in range(100):
+ # Print their name followed by a space, not a new line
+ print("I love you", name, end = " ")
