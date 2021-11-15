70c92640bdc75bd1b6e501692ec78dca750d4b47
Kwaku Opoku-Ware <94206249+Kwakuopokuware401@users.noreply.github.com>
2021-11-15 16:23:18 +0100

    Update turtle_game.py

--- turtle_game.py
+++ turtle_game.py
@@ -1,5 +1,11 @@
+#Import in-built function turtle from your python
 import turtle
+
+
+#naming a variable from drawing 
 t = turtle.Pen()
+
+#Creating for loops
 for x in range(100):
     t.forward(x)
     t.left(90)
